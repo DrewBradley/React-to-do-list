3bb29000e208c9861123d207ab041892800d31c4
Drew Bradley <williamandrewb@gmail.com>
2021-01-28 19:31:45 -0700

    Updates

---
+++ precache-manifest.c6246ad0b12a5091f11f793c16a17924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7330e49f042bc8eb8b195d8146a867d",
+    "url": "/React-to-do-list/index.html"
+  },
+  {
+    "revision": "556a7d364ed45a003d0f",
+    "url": "/React-to-do-list/static/css/main.fdc1d5a3.chunk.css"
+  },
+  {
+    "revision": "a5d1bf071d44911d7e35",
+    "url": "/React-to-do-list/static/js/2.4d805596.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-to-do-list/static/js/2.4d805596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556a7d364ed45a003d0f",
+    "url": "/React-to-do-list/static/js/main.4834b895.chunk.js"
+  },
+  {
+    "revision": "d9e9475f96ee858bfb45",
+    "url": "/React-to-do-list/static/js/runtime-main.452f1c60.js"
+  }
+]);
